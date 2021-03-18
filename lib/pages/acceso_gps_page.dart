b5b373c9e3323d1e887bf1a9e2fd8db78db63cb9
Johan David <62624815+johandavidcm@users.noreply.github.com>
2021-03-18 17:49:45 -0500

    Primera seccion terminada

---
+++ lib/pages/acceso_gps_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class AccesoGpsPage extends StatefulWidget {
+  @override
+  _AccesoGpsPageState createState() => _AccesoGpsPageState();
+}
+
+class _AccesoGpsPageState extends State<AccesoGpsPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed) {
+      if (await Permission.location.isGranted) {
+        Navigator.pushReplacementNamed(context, 'loading');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Es necesario el GPS para usar esta app'),
+            MaterialButton(
+                color: Colors.black,
+                shape: StadiumBorder(),
+                elevation: 0,
+                splashColor: Colors.transparent,
+                child: Text(
+                  'Solicitar accesso',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  //TODO: Verificar Permisos
+                  final status = await Permission.location.request();
+                  this.accesoGPS(status);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  void accesoGPS(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.granted:
+        Navigator.pushReplacementNamed(context, 'mapa');
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.limited:
+      case PermissionStatus.permanentlyDenied:
+        openAppSettings();
+    }
+  }
+}
---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:map_app/helpers/helpers.dart';
+import 'package:map_app/pages/acceso_gps_page.dart';
+import 'package:map_app/pages/mapa_page.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed) {
+      if (await Geolocator.isLocationServiceEnabled()) {
+        Navigator.pushReplacement(
+            context, navegarMapaFadeIn(context, MapaPage()));
+      }
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: this.checkGpsYLocation(context),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          print(snapshot.data);
+          if (snapshot.hasData) {
+            return Center(
+              child: Text(snapshot.data),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Future checkGpsYLocation(BuildContext context) async {
+    //PermisoGPS
+    final permisoGPS = await Permission.location.isGranted;
+    // GPS Activo
+    final gpsActivo = await Geolocator.isLocationServiceEnabled();
+    if (permisoGPS && gpsActivo) {
+      Navigator.pushReplacement(
+          context, navegarMapaFadeIn(context, MapaPage()));
+      return '';
+    } else if (!permisoGPS) {
+      Navigator.pushReplacement(
+          context, navegarMapaFadeIn(context, AccesoGpsPage()));
+      return 'Es necesario el permiso del GPS';
+    } else {
+      return 'active el GPS';
+    }
+  }
+}
